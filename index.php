d2ff2a0c5ca5507e97b6eaa85d592d3405dcceb0
Alvaro <alvaroroi66@gmail.com>
2026-05-12 21:32:12 +0200

    re

--- index.php
+++ index.php
@@ -83,5 +83,29 @@
             }
         </style>
     </head>
-    
+    <body>
+        <?php
+        session_start();
+        include 'vues/v-nav.php';
+
+        if (!isset($_REQUEST['uc'])) {
+            $_REQUEST['uc'] = 'public';
+        }
+
+        $uc = htmlspecialchars($_REQUEST["uc"]);
+        switch ($uc) {
+            case 'public':
+                include 'controleurs/ControleurPublic.php';
+                break;
+            default:
+                include 'vues/v-accueil.php';
+                break;
+        }
+        include 'vues/v-footer.php';
+        ?>
+        <!-- Bootstrap JS -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    </body>
 </html>
